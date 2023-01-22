e3584883ff85e48c7deb563aa0223278dfbcf678
Andres6936 <joan.buritica@artemisa.tech>
2023-01-22 17:52:09 -0500

    [Branch] Server Side Rendering

--- src/Template.ts
+++ src/Template.ts
@@ -6,27 +6,6 @@ export function Template() {
     }, [])
 
     return (
-        React.createElement('html', {
-            children: [
-                React.createElement('head', {
-                    children: [
-                        React.createElement('meta', {
-                            charSet: 'utf-8'
-                        }),
-                        React.createElement('meta', {
-                            name: "viewport",
-                            content: "width=device-width, initial-scale=1",
-                        }),
-                        React.createElement('link', {
-                            rel: "stylesheet",
-                            href: "styles.css"
-                        }),
-                        React.createElement('title', {}, 'SSR React')
-                    ]
-                }),
-                React.createElement('body', {
-                    children: [React.createElement('h1', null, 'Express + TypeScript Server + React Server Side Render')]
-                })]
-        })
+        React.createElement('h1', null, 'Express + TypeScript Server + React Server Side Render')
     )
 }
